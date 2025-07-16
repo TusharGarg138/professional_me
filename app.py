1deeb0dd49e3d7c0c33d7b3455202654b56bc11e
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-07-17 00:03:46 +0530

    importing libs

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+import os
+import requests
+from pypdf import PdfReader
+import gradio as gr
+import google.generativeai as genai
